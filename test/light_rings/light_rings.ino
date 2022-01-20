6ba3569b43850b2ae8790f3b48b56c485ee3e812
Thomas van Klink <72467220+thomasvklink@users.noreply.github.com>
2022-01-21 00:26:25 +0100

    LED functionality and button input
    Working input and first game.
    Also added file with pseudo code to write the games (by Tristan van Marle)

--- test/light_rings/light_rings.ino
+++ test/light_rings/light_rings.ino
@@ -1,10 +1,16 @@
 //Project module 6 Create VER-WER-PE-LIJK
 //This code is created and adapted by the VER-WER-PE-LIJK team to be able to play games using connected esp8266's and let the modules react to the game states
 //the espnow- section of this code is largely inspired by https://github.com/bnbe-club/esp-now-examples-diy-62 which is under the creative commons license 'CC0 1.0 Universal'
+//Fade in / Fade out LED animations inspired by https://gist.github.com/marmilicious/f86b39d8991e1efcfd9fbd90dcdf751b made by Scott Kletzien
 
+//ESP Now Connectifity
 #include<ESP8266WiFi.h>
 #include<espnow.h>
 
+//LED libary to control LED Strip
+#include <FastLED.h>
+
+//ESP NOW Functionality
 #define MY_ROLE         ESP_NOW_ROLE_COMBO              // set the role of this device: CONTROLLER, SLAVE, COMBO
 #define RECEIVER_ROLE   ESP_NOW_ROLE_COMBO              // set the role of the receiver
 #define WIFI_CHANNEL    1
@@ -44,13 +50,31 @@ void dataReceived(uint8_t *senderMac, uint8_t *data, uint8_t dataLength) {
   Serial.println(packet.gameState);
 }
 
-const int buttonPin = 4; 
- 
+// Set pins for switches
+const int buttonPin = 4;
+int buttonState = 0;
+boolean pressed;
+// Set pins and LED number
+const int NUM_LEDS = 51;
+const int DATA_PIN = 2;
+CRGB leds[NUM_LEDS];
+int effect;
+int brightness;
+int fadeBrightness = 0;
+bool setColor; 
+
+unsigned long previousMillis = 0;
+
 void setup() {
   Serial.begin(115200);     // initialize serial port
-
+  // initialize the pushbutton pin as an input
   pinMode(buttonPin, INPUT);
+  // Initialize FastLED on pin
+  FastLED.addLeds<WS2811, 2, GRB>(leds, NUM_LEDS);
+  effect = 4;
+  brightness = 50;
 
+  //ESP NOW
   Serial.println();
   Serial.println();
   Serial.println();
@@ -62,29 +86,37 @@ void setup() {
 
   if(esp_now_init() != 0) {
     Serial.println("ESP-NOW initialization failed");
+   // effect = 1 //LED effect that no connection had been made
     return;
-  }
+  } else {
+   // effect = 2 //LED effect that connection has been made
+    }
 
   esp_now_set_self_role(MY_ROLE);   
   esp_now_register_send_cb(transmissionComplete);         // this function will get called once all data is sent
   esp_now_register_recv_cb(dataReceived);               // this function will get called whenever we receive data
   esp_now_add_peer(receiverAddress, RECEIVER_ROLE, WIFI_CHANNEL, NULL, 0);
 
-  Serial.println("Initialized.");
+  Serial.println("ESP-NOW Initialized.");
 }
 
 void loop() {
   detect();
-  send_packet();
+  control();
+  
+  unsigned long currentMillis = millis();  
+  if (currentMillis - previousMillis >= 3000) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    send_packet();
+  }
 }
 
 void send_packet(){
   dataPacket packet;
-  
   packet.pressed=false;
   packet.gameState=0;
   esp_now_send(receiverAddress, (uint8_t *) &packet, sizeof(packet));
-  delay(3000);
 }
 
 void detect(){
@@ -93,10 +125,63 @@ void detect(){
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH
   if (buttonState == HIGH) {
-    digitalWrite(indicatorPin, HIGH);
     pressed = true;
   } else {
-    digitalWrite(indicatorPin, LOW);
     //pressed = false;
+  }
+}
+
+void control(){
+  FastLED.setBrightness(brightness);
+  switch(effect){
+    // Disconnected state - Red blinking
+    case 1:{
+      for (int i = 0; i <= NUM_LEDS; i++) {
+        leds[i] = CRGB::Red; 
+      }
+      FastLED.show();
+      delay(1000);
+      for (int i = 0; i <= NUM_LEDS; i++) {
+        leds[i] = CRGB::Black; 
+      }
+      FastLED.show();
+      delay(1000);
+    }break;
+    // Connected state - Green fade in
+    case 2:{
+     int fadeAmount = 1;
+     for(int i = 0; i < NUM_LEDS; i++ ){
+       leds[i].setRGB(0,255,0);
+       leds[i].fadeLightBy(fadeBrightness);
+      }
+      FastLED.show();
+      fadeBrightness = fadeBrightness + fadeAmount;
+      // reverse the direction of the fading at the ends of the fade: 
+      if(fadeBrightness == 0 || fadeBrightness == 255){
+        fadeAmount = -fadeAmount ; 
+      }    
+      delay(10);  // Speed of the fade
+    }break;
+    // Idle state - LED trailing back and forth
+    case 3:{
+      fadeToBlackBy( leds, NUM_LEDS, 1);
+      int pos = beatsin16(5, 0, NUM_LEDS-1);
+      leds[pos] += CHSV( 0, 255, 192);
+      FastLED.show();
+    }break;
+    //Game 1 - If target is hit change to random color
+    case 4:{
+    if (!setColor){
+      int randomColor = random8();
+        for (int i = 0; i <= NUM_LEDS; i++) {
+          leds[i] = CHSV(randomColor,255,255);
+          setColor = true; 
+        }
+      }  
+    FastLED.show();
+     if(pressed){
+      setColor = false;
+      }
+    }break;
   }
 }
